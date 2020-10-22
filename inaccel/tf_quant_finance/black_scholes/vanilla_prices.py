00f0a4d085025d1a69d483ebd996fffb282c0a88
Elias Koromilas <elias.koromilas@gmail.com>
2020-10-22 10:39:56 +0300

    Add option and binary vanilla prices reference

---
+++ inaccel/tf_quant_finance/black_scholes/vanilla_prices.py
@@ -0,0 +1,323 @@
+# Lint as: python3
+# Copyright 2019 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Black Scholes prices of a batch of European options."""
+
+import numpy as np
+import tensorflow.compat.v2 as tf
+
+
+def option_price(*,
+                 volatilities,
+                 strikes,
+                 expiries,
+                 spots=None,
+                 forwards=None,
+                 discount_rates=None,
+                 continuous_dividends=None,
+                 cost_of_carries=None,
+                 discount_factors=None,
+                 is_call_options=None,
+                 is_normal_volatility=False,
+                 dtype=None,
+                 name=None):
+  """Computes the Black Scholes price for a batch of call or put options.
+
+  #### Example
+
+  ```python
+    # Price a batch of 5 vanilla call options.
+    volatilities = np.array([0.0001, 102.0, 2.0, 0.1, 0.4])
+    forwards = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+    # Strikes will automatically be broadcasted to shape [5].
+    strikes = np.array([3.0])
+    # Expiries will be broadcast to shape [5], i.e. each option has strike=3
+    # and expiry = 1.
+    expiries = 1.0
+    computed_prices = tff.black_scholes.option_price(
+        volatilities=volatilities,
+        strikes=strikes,
+        expiries=expiries,
+        forwards=forwards)
+  # Expected print output of computed prices:
+  # [ 0.          2.          2.04806848  1.00020297  2.07303131]
+  ```
+
+  #### References:
+  [1] Hull, John C., Options, Futures and Other Derivatives. Pearson, 2018.
+  [2] Wikipedia contributors. Black-Scholes model. Available at:
+    https://en.wikipedia.org/w/index.php?title=Black%E2%80%93Scholes_model
+
+  Args:
+    volatilities: Real `Tensor` of any shape and dtype. The volatilities to
+      expiry of the options to price.
+    strikes: A real `Tensor` of the same dtype and compatible shape as
+      `volatilities`. The strikes of the options to be priced.
+    expiries: A real `Tensor` of same dtype and compatible shape as
+      `volatilities`. The expiry of each option. The units should be such that
+      `expiry * volatility**2` is dimensionless.
+    spots: A real `Tensor` of any shape that broadcasts to the shape of the
+      `volatilities`. The current spot price of the underlying. Either this
+      argument or the `forwards` (but not both) must be supplied.
+    forwards: A real `Tensor` of any shape that broadcasts to the shape of
+      `volatilities`. The forwards to maturity. Either this argument or the
+      `spots` must be supplied but both must not be supplied.
+    discount_rates: An optional real `Tensor` of same dtype as the
+      `volatilities` and of the shape that broadcasts with `volatilities`.
+      If not `None`, discount factors are calculated as e^(-rT),
+      where r are the discount rates, or risk free rates. At most one of
+      discount_rates and discount_factors can be supplied.
+      Default value: `None`, equivalent to r = 0 and discount factors = 1 when
+      discount_factors also not given.
+    continuous_dividends: An optional real `Tensor` of same dtype as the
+      `volatilities` and of the shape that broadcasts with `volatilities`.
+      If not `None`, `cost_of_carries` is calculated as r - q,
+      where r are the `discount_rates` and q is `continuous_dividends`. Either
+      this or `cost_of_carries` can be given.
+      Default value: `None`, equivalent to q = 0.
+    cost_of_carries: An optional real `Tensor` of same dtype as the
+      `volatilities` and of the shape that broadcasts with `volatilities`.
+      Cost of storing a physical commodity, the cost of interest paid when
+      long, or the opportunity cost, or the cost of paying dividends when short.
+      If not `None`, and `spots` is supplied, used to calculate forwards from
+      `spots`: F = e^(bT) * S, where F is the forwards price, b is the cost of
+      carries, T is expiries and S is the spot price. If `None`, value assumed
+      to be equal to the `discount_rate` - `continuous_dividends`
+      Default value: `None`, equivalent to b = r.
+    discount_factors: An optional real `Tensor` of same dtype as the
+      `volatilities`. If not `None`, these are the discount factors to expiry
+      (i.e. e^(-rT)). Mutually exclusive with discount_rate and cost_of_carry.
+      If neither is given, no discounting is applied (i.e. the undiscounted
+      option price is returned). If `spots` is supplied and `discount_factors`
+      is not `None` then this is also used to compute the forwards to expiry.
+      At most one of discount_rates and discount_factors can be supplied.
+      Default value: `None`, which maps to -log(discount_factors) / expiries
+    is_call_options: A boolean `Tensor` of a shape compatible with
+      `volatilities`. Indicates whether the option is a call (if True) or a put
+      (if False). If not supplied, call options are assumed.
+    is_normal_volatility: An optional Python boolean specifying whether the
+      `volatilities` correspond to lognormal Black volatility (if False) or
+      normal Black volatility (if True).
+      Default value: False, which corresponds to lognormal volatility.
+    dtype: Optional `tf.DType`. If supplied, the dtype to be used for conversion
+      of any supplied non-`Tensor` arguments to `Tensor`.
+      Default value: `None` which maps to the default dtype inferred by
+        TensorFlow.
+    name: str. The name for the ops created by this function.
+      Default value: `None` which is mapped to the default name `option_price`.
+
+  Returns:
+    option_prices: A `Tensor` of the same shape as `forwards`. The Black
+    Scholes price of the options.
+
+  Raises:
+    ValueError: If both `forwards` and `spots` are supplied or if neither is
+      supplied.
+    ValueError: If both `discount_rates` and `discount_factors` is supplied.
+    ValueError: If both `continuous_dividends` and `cost_of_carries` is
+      supplied.
+  """
+  if (spots is None) == (forwards is None):
+    raise ValueError('Either spots or forwards must be supplied but not both.')
+  if (discount_rates is not None) and (discount_factors is not None):
+    raise ValueError('At most one of discount_rates and discount_factors may '
+                     'be supplied')
+  if (continuous_dividends is not None) and (cost_of_carries is not None):
+    raise ValueError('At most one of continuous_dividends and cost_of_carries '
+                     'may be supplied')
+
+  with tf.name_scope(name or 'option_price'):
+    strikes = tf.convert_to_tensor(strikes, dtype=dtype, name='strikes')
+    dtype = strikes.dtype
+    volatilities = tf.convert_to_tensor(
+        volatilities, dtype=dtype, name='volatilities')
+    expiries = tf.convert_to_tensor(expiries, dtype=dtype, name='expiries')
+
+    if discount_rates is not None:
+      discount_rates = tf.convert_to_tensor(
+          discount_rates, dtype=dtype, name='discount_rates')
+    elif discount_factors is not None:
+      discount_rates = -tf.math.log(discount_factors) / expiries
+    else:
+      discount_rates = tf.convert_to_tensor(
+          0.0, dtype=dtype, name='discount_rates')
+
+    if continuous_dividends is None:
+      continuous_dividends = tf.convert_to_tensor(
+          0.0, dtype=dtype, name='continuous_dividends')
+
+    if cost_of_carries is not None:
+      cost_of_carries = tf.convert_to_tensor(
+          cost_of_carries, dtype=dtype, name='cost_of_carries')
+    else:
+      cost_of_carries = discount_rates - continuous_dividends
+
+    if discount_factors is not None:
+      discount_factors = tf.convert_to_tensor(
+          discount_factors, dtype=dtype, name='discount_factors')
+    else:
+      discount_factors = tf.exp(-discount_rates * expiries)
+
+    if forwards is not None:
+      forwards = tf.convert_to_tensor(forwards, dtype=dtype, name='forwards')
+    else:
+      spots = tf.convert_to_tensor(spots, dtype=dtype, name='spots')
+      forwards = spots * tf.exp(cost_of_carries * expiries)
+
+    sqrt_var = volatilities * tf.math.sqrt(expiries)
+    if not is_normal_volatility:  # lognormal model
+      d1 = (tf.math.log(forwards / strikes) +
+            sqrt_var * sqrt_var / 2) / sqrt_var
+      d2 = d1 - sqrt_var
+      undiscounted_calls = forwards * _ncdf(d1) - strikes * _ncdf(d2)
+    else:  # normal model
+      d1 = (forwards - strikes) / sqrt_var
+      undiscounted_calls = (forwards - strikes) * _ncdf(
+          d1) + sqrt_var * tf.math.exp(-0.5 * d1**2) / np.sqrt(2 * np.pi)
+
+    if is_call_options is None:
+      return discount_factors * undiscounted_calls
+    undiscounted_forward = forwards - strikes
+    undiscounted_puts = undiscounted_calls - undiscounted_forward
+    predicate = tf.broadcast_to(is_call_options, tf.shape(undiscounted_calls))
+    return discount_factors * tf.where(predicate, undiscounted_calls,
+                                       undiscounted_puts)
+
+
+# TODO(b/154806390): Binary price signature should be the same as that of the
+# vanilla price.
+def binary_price(*,
+                 volatilities,
+                 strikes,
+                 expiries,
+                 spots=None,
+                 forwards=None,
+                 discount_factors=None,
+                 is_call_options=None,
+                 dtype=None,
+                 name=None):
+  """Computes the Black Scholes price for a batch of binary call or put options.
+
+  The binary call (resp. put) option priced here is that which pays off a unit
+  of cash if the underlying asset has a value greater (resp. smaller) than the
+  strike price at expiry. Hence the binary option price is the discounted
+  probability that the asset will end up higher (resp. lower) than the
+  strike price at expiry.
+
+  #### Example
+
+  ```python
+    # Price a batch of 5 binary call options.
+    volatilities = np.array([0.0001, 102.0, 2.0, 0.1, 0.4])
+    forwards = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+    # Strikes will automatically be broadcasted to shape [5].
+    strikes = np.array([3.0])
+    # Expiries will be broadcast to shape [5], i.e. each option has strike=3
+    # and expiry = 1.
+    expiries = 1.0
+    computed_prices = tff.black_scholes.binary_price(
+        volatilities=volatilities,
+        strikes=strikes,
+        expiries=expiries,
+        forwards=forwards)
+  # Expected print output of prices:
+  # [0.         0.         0.15865525 0.99764937 0.85927418]
+  ```
+
+  #### References:
+
+  [1] Hull, John C., Options, Futures and Other Derivatives. Pearson, 2018.
+  [2] Wikipedia contributors. Binary option. Available at:
+  https://en.wikipedia.org/w/index.php?title=Binary_option
+
+  Args:
+    volatilities: Real `Tensor` of any shape and dtype. The volatilities to
+      expiry of the options to price.
+    strikes: A real `Tensor` of the same dtype and compatible shape as
+      `volatilities`. The strikes of the options to be priced.
+    expiries: A real `Tensor` of same dtype and compatible shape as
+      `volatilities`. The expiry of each option. The units should be such that
+      `expiry * volatility**2` is dimensionless.
+    spots: A real `Tensor` of any shape that broadcasts to the shape of the
+      `volatilities`. The current spot price of the underlying. Either this
+      argument or the `forwards` (but not both) must be supplied.
+    forwards: A real `Tensor` of any shape that broadcasts to the shape of
+      `volatilities`. The forwards to maturity. Either this argument or the
+      `spots` must be supplied but both must not be supplied.
+    discount_factors: An optional real `Tensor` of same dtype as the
+      `volatilities`. If not None, these are the discount factors to expiry
+      (i.e. e^(-rT)). If None, no discounting is applied (i.e. the undiscounted
+      option price is returned). If `spots` is supplied and `discount_factors`
+      is not None then this is also used to compute the forwards to expiry.
+      Default value: None, equivalent to discount factors = 1.
+    is_call_options: A boolean `Tensor` of a shape compatible with
+      `volatilities`. Indicates whether the option is a call (if True) or a put
+      (if False). If not supplied, call options are assumed.
+    dtype: Optional `tf.DType`. If supplied, the dtype to be used for conversion
+      of any supplied non-`Tensor` arguments to `Tensor`.
+      Default value: None which maps to the default dtype inferred by TensorFlow
+        (float32).
+    name: str. The name for the ops created by this function.
+      Default value: None which is mapped to the default name `binary_price`.
+
+  Returns:
+    binary_prices: A `Tensor` of the same shape as `forwards`. The Black
+    Scholes price of the binary options.
+
+  Raises:
+    ValueError: If both `forwards` and `spots` are supplied or if neither is
+      supplied.
+  """
+  if (spots is None) == (forwards is None):
+    raise ValueError('Either spots or forwards must be supplied but not both.')
+
+  with tf.name_scope(name or 'binary_price'):
+    strikes = tf.convert_to_tensor(strikes, dtype=dtype, name='strikes')
+    dtype = strikes.dtype
+    volatilities = tf.convert_to_tensor(
+        volatilities, dtype=dtype, name='volatilities')
+    expiries = tf.convert_to_tensor(expiries, dtype=dtype, name='expiries')
+
+    if discount_factors is None:
+      discount_factors = tf.convert_to_tensor(
+          1.0, dtype=dtype, name='discount_factors')
+    else:
+      discount_factors = tf.convert_to_tensor(
+          discount_factors, dtype=dtype, name='discount_factors')
+
+    if forwards is not None:
+      forwards = tf.convert_to_tensor(forwards, dtype=dtype, name='forwards')
+    else:
+      spots = tf.convert_to_tensor(spots, dtype=dtype, name='spots')
+      forwards = spots / discount_factors
+
+    sqrt_var = volatilities * tf.math.sqrt(expiries)
+    d1 = (tf.math.log(forwards / strikes) + sqrt_var * sqrt_var / 2) / sqrt_var
+    d2 = d1 - sqrt_var
+    undiscounted_calls = _ncdf(d2)
+    if is_call_options is None:
+      return discount_factors * undiscounted_calls
+    is_call_options = tf.convert_to_tensor(is_call_options,
+                                           dtype=tf.bool,
+                                           name='is_call_options')
+    undiscounted_puts = 1 - undiscounted_calls
+    predicate = tf.broadcast_to(is_call_options, tf.shape(undiscounted_calls))
+    return discount_factors * tf.where(predicate, undiscounted_calls,
+                                       undiscounted_puts)
+
+
+def _ncdf(x):
+  return (tf.math.erf(x / _SQRT_2) + 1) / 2
+
+
+_SQRT_2 = np.sqrt(2.0, dtype=np.float64)
